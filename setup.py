b55acc79b0a5319dd119c45eeb42c6182cbe1598
Arjun Patel <arjun611@arjuns-air.home>
2024-02-06 21:14:32 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+
+
+setup(
+    name='DiamondPriceprediction',
+    version='0.0.1',
+    author='Arjun611',
+    author_email='agharaarjun611@gmail.com',
+    install_requires=['pandas','sklearn'],
+    packages=find_packages()
+    
+)
